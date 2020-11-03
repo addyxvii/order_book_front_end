34f9868ba5e86d2d6c5ac05f24c3b083c2ba605a
addyxvii <addisonbennett@gmail.com>
2020-11-02 18:09:08 -0700

    sweet data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { EffectCallback, useEffect, useState } from 'react';
+import openSocket from 'socket.io-client';
 import './App.css';
 
+const ENDPOINT = "http://localhost:8000";
+const socket = openSocket(ENDPOINT);
+
 function App() {
+
+  // put bittrex state here
+  const [bittrexState, setBittrexState] = useState<any>(undefined)
+  const [poloniexState, setPoloniexState] = useState(undefined)
+
+  // get poloniex
+  useEffect(() => {
+    socket.emit('fetchPolinexData', {}, (error: any) => {
+      console.log("yooooo")
+    });
+  }, []);
+
+  // recieve poloniex data
+  useEffect(() => {
+    socket.on('recievePoloniexData', (poloniexData: any) => {
+      // set bittrex state here
+      if (poloniexState === undefined) {
+        console.log('got poloniex')
+        setPoloniexState(poloniexData)
+      } 
+    }); 
+    return () => socket.disconnect() as any;
+  }, []);
+  
+  // open up connection to your socket API here
+  useEffect(() => {
+    socket.emit('fetchBittrexData', { optionalPayload: null }, (error: any) => {
+      console.log('request sent to API to fetch bittrex data')
+    });
+  },[]);
+
+  // listen for responses from your socket API here and update state in react
+  useEffect(() => {
+    socket.on('recieveBittrexData', (bittrexData: any) => {
+      console.log(bittrexData)
+      setBittrexState(bittrexData)
+      
+    });
+    return () => socket.disconnect() as any;
+  }, []);
+
   return (
     <div className="App">
       <h1>Order Book </h1>
+      {
+        bittrexState ? "yay data" : 'nodataa'
+      }
+            {
+        poloniexState ? "yay poloniex data" : 'no poloniex dataa'
+      }
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import openSocket from 'socket.io-client';
-import './App.css';
+import React from 'react'
+import App from './App'
+import ReactDOM from 'react-dom'
 
-const ENDPOINT = "http://localhost:8000";
-const socket = openSocket(ENDPOINT);
-
-function App() {
-
-  // put bittrex state here
-  const [bittrexState, setBittrexState] = useState()
-
-  // open up connection to your socket API here
-  useEffect(() => {
-    console.log("APP LOADED")
-    socket.emit('fetchBittrexData', { optionalPayload: null }, (error: any) => {
-      console.log('request sent to API to fetch bittrex data')
-    });
-  },);
-
-  // listen for responses from your socket API here and update state in react
-  useEffect(() => {
-
-    socket.on('recieveBittrexData', (bittrexData: any) => {
-      console.log('Got response from api for bittrex data')
-
-      // set bittrex state here
-      setBittrexState(bittrexData)
-    });
-
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Order Book </h1>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+      <App />,
+  document.getElementById('root')
+);
