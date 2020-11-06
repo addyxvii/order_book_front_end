0dc287177d419726c0211e032022c46b39df8523
addyxvii <addisonbennett@gmail.com>
2020-11-05 17:08:16 -0700

    wip: adds table body logic

--- src/Book.tsx
+++ src/Book.tsx
@@ -1,30 +1,51 @@
 import React, { FunctionComponent } from 'react';
+import './Book.css';
 
-type TableProps = {
+
+interface TableProps {
     title: string,
-    exchange?: string,
-    rate?: string,
-    amount?: string,
-
+    data: any[]
 }
- const Book: FunctionComponent<TableProps> = ({ title, exchange, rate, amount }) => {
-return (
-<table>
- <tr>
-    {title}
- </tr>
- <tr>
-    <th> Exchange </th>
-    <th> Rate </th>
-    <th> Amount </th>
-</tr>
- <tr>
-    <td>{exchange}</td>
-    <td>{rate}</td>
-    <td>{amount}</td>
- </tr>
-</table> 
-)}
+
+const Book: FunctionComponent<TableProps> = ({
+    title,
+    data
+}) => {
+    console.log(data.length)
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td>{title}</td>
+                </tr>
+            </thead>
+            <thead>
+                <tr>
+                    <th> Exchange </th>
+                    <th> Rate </th>
+                    <th> Amount </th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data && data.length && data.length > 8 ? data.map((row: any) => {
+                        return (
+                            <tr>
+                                {
+                                    Object.values(row).map((item: any) => {
+                                        return (
+                                            <td>{item}</td>
+                                        )
+                                    })
+                                }
+                            </tr>
+                        )
+                    }) : "loading table..."
+                }
+            </tbody>
+        </table>
+    )
+}
 
 export default Book;
 
